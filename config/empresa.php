15f62f3ac3c32cc1464e032c338fa3dcfed986ca
YouAreMySecondHemisphere17 <derekcaguaallneonlike@gmail.com>
2025-06-21 21:00:37 -0500

    factura

---
+++ config/empresa.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'nombre' => 'Papelería El Lápiz Veloz',
+    'ruc' => '1790012345001',
+    'direccion' => 'Guayaquil, Ecuador',
+    'telefono' => '(04) 222-3333',
+    'email' => 'contacto@lapizveloz.com',
+
+    'eslogan' => 'Todo en útiles escolares y materiales de oficina.',
+    'ciudad' => 'Guayaquil'
+];
